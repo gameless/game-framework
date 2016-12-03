4dd64571e2806b078f08e8d5eef83a25036e98b1
James Pruitt <jamescpruitt@gmail.com>
2016-12-03 18:07:32 -0500

    made sprite more unified with piston

--- src/sprite.rs
+++ src/sprite.rs
@@ -7,22 +7,35 @@ use graphics::image;
 use opengl_graphics::GlGraphics;
 use opengl_graphics::Texture;
 use graphics::Transformed;
+use graphics::ImageSize;
 
 pub struct Sprite {
     tex: Texture,
-    pub pos: [f64; 2],
+    pub pos: (f64, f64),
 }
 
 impl Sprite {
     pub fn new(filename: &str) -> Sprite {
         Sprite {
             tex: load_img(filename),
-            pos: [0.0, 0.0],
+            pos: (0.0, 0.0),
         }
     }
 
     pub fn draw(&self, c: &Context, gl: &mut GlGraphics) {
         // draw stuff
-        image(&self.tex, c.transform.trans(self.pos[0], self.pos[1]), gl);
+        image(&self.tex, c.transform.trans(self.pos.0, self.pos.1), gl);
+    }
+
+    pub fn get_size(&self) -> (u32, u32) {
+        self.tex.get_size()
+    }
+
+    pub fn get_width(&self) -> u32 {
+        self.tex.get_width()
+    }
+
+    pub fn get_height(&self) -> u32 {
+        self.tex.get_height()
     }
 }
